b376cb4c989b8ca77d253080148d2a8a30cfb576
purthi <132135140+purthi@users.noreply.github.com>
2026-02-11 16:23:29 +0530

    Create app.java

---
+++ src/main/java/com/example/app.java
@@ -0,0 +1,8 @@
+/**
+ * A simple Java application to demonstrate Maven project structure.
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, Maven World!");
+    }
+}
